8ecb3a6441c8f905b0b93894f7f378456a38f363
Henry Chavez <henry19.chavez@gmail.com>
2020-03-31 12:40:44 -0500

    chore: initial commit

---
+++ demo/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+
+import { ItemDetailComponent } from './item/item-detail.component';
+import { ItemsComponent } from './item/items.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: 'items', component: ItemsComponent },
+  { path: 'item/:id', component: ItemDetailComponent },
+];
+
+@NgModule({
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule],
+})
+export class AppRoutingModule {}
